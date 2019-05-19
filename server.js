fecac3dd57ce5f59854dc29ae88c86f786ed1aa9
Phattaw <phattaw@gmail.com>
2019-05-19 17:26:25 -0500

    Add process.env.PORT ||

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var app = express();
 
 // Sets up the Express App
 // =============================================================
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
